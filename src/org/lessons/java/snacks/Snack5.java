8ce0e6e363603ed581ff4029f2545801b41376fb
LorisMarzocchi <ml77182a@gmail.com>
2023-10-19 16:45:47 +0200

    snack 4

--- src/org/lessons/java/snacks/Snack5.java
+++ src/org/lessons/java/snacks/Snack5.java
@@ -2,16 +2,15 @@ package org.lessons.java.snacks;
 
 public class Snack5 {
     public static void main(String[] args) {
-        String numeroStr = "20";
+        String numeroStr = "25";
         int numeroInt = 0;
 
         for (int i = 0; i < numeroStr.length(); i++) {
-            // Ottiene il valore numerico del carattere corrente
+
             int valoreCorrente = numeroStr.charAt(i) - '0';
+
             System.out.println(valoreCorrente);
 
-            // Aggiorna numeroInt moltiplicandolo per 10 (sposta il numero di una posizione)
-            // e aggiungendo il valoreCorrente
             numeroInt = numeroInt * 10 + valoreCorrente;
         }
 
---
+++ src/org/lessons/java/snacks/Snack6.java
@@ -0,0 +1,7 @@
+package org.lessons.java.snacks;
+
+public class Snack6 {
+    public static void main(String[] args) {
+
+    }
+}
