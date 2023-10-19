50047d492eb7023a7d293135f5bcbbf855fd0f5a
LorisMarzocchi <ml77182a@gmail.com>
2023-10-19 16:32:03 +0200

    snack 4

--- src/org/lessons/java/snacks/Snack4.java
+++ src/org/lessons/java/snacks/Snack4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Snack4 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+/*        Scanner scan = new Scanner(System.in);
         System.out.println("inserisci una parola per verificare se è palindroma");
         String parola = scan.nextLine().toLowerCase();
 
@@ -13,6 +13,21 @@ public class Snack4 {
             reverseWord += (parola.charAt(i));
         }
         if (parola.equals(reverseWord)) {
+            System.out.println("La parola è palindroma.");
+        } else {
+            System.out.println("La parola non è palindroma.");
+        }*/
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("inserisci una parola per verificare se è palindroma");
+        String parola = scan.nextLine().toLowerCase();
+
+        StringBuilder reverseWord = new StringBuilder();
+        for (int i = parola.length() - 1; i >= 0; i--) {
+            reverseWord.append(parola.charAt(i));
+        }
+        if (parola.contentEquals(reverseWord)) {
             System.out.println("La parola è palindroma.");
         } else {
             System.out.println("La parola non è palindroma.");
--- src/org/lessons/java/snacks/Snack5.java
+++ src/org/lessons/java/snacks/Snack5.java
@@ -2,6 +2,19 @@ package org.lessons.java.snacks;
 
 public class Snack5 {
     public static void main(String[] args) {
+        String numeroStr = "20";
+        int numeroInt = 0;
 
+        for (int i = 0; i < numeroStr.length(); i++) {
+            // Ottiene il valore numerico del carattere corrente
+            int valoreCorrente = numeroStr.charAt(i) - '0';
+            System.out.println(valoreCorrente);
+
+            // Aggiorna numeroInt moltiplicandolo per 10 (sposta il numero di una posizione)
+            // e aggiungendo il valoreCorrente
+            numeroInt = numeroInt * 10 + valoreCorrente;
+        }
+
+        System.out.println("Il numero convertito è: " + numeroInt);
     }
 }
